52b193ba6faff27d604938f5520d35c88e05e8c9
Fracuzzi <jhonfrankx2811s@gmail.com>
2024-10-16 14:19:05 -0500

    cerrar la session del usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -100,5 +100,14 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+	}
+	
+	
 
 }
